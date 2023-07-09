4cb17591f7eb2c1169d5bbc22a06ac79dff2fbbd
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-07-09 12:39:32 +0300

    Connect mongoDb, implement getMongoConfig and getMongoString

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: 'envs/.account.env' }),
+    MongooseModule.forRootAsync(getMongoConfig()),
     UserModule,
     AuthModule,
   ],
