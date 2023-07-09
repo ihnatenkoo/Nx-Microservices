5c10efd263ee4d5671804675eef5800bbba363a5
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-07-09 15:19:15 +0300

    Create getJWTConfig

--- apps/account/src/app/auth/auth.module.ts
+++ apps/account/src/app/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserRepository } from '../user/repositories/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { getJWTConfig } from '../configs/jwt.config';
 
 @Module({
+  imports: [UserRepository, JwtModule.registerAsync(getJWTConfig())],
   controllers: [AuthController],
   providers: [AuthService],
 })
