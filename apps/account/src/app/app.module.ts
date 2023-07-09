53822c88f4012467d3850093165c275158c32ff5
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-07-09 12:09:41 +0300

    Connect ConfigModule

--- apps/account/src/app/app.module.ts
+++ apps/account/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: 'envs/.account.env' }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
