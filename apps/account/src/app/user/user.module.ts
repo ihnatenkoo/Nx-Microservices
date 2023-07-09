ed5fdc33eda7e666ec3f13f0976a5821bbb2513d
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-07-09 13:09:34 +0300

    Create UserEntity

--- apps/account/src/app/user/user.module.ts
+++ apps/account/src/app/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModel, UserSchema } from './models/user.model';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: UserModel.name, schema: UserSchema }]),
+  ],
   providers: [UserService],
 })
 export class UserModule {}
